547dc4210a5ae78ca45d4b55aa51bf7f9a41656e
cindyzhu <zhubaiying@rong360.com>
2019-04-11 20:17:03 +0800

    m files

--- build/prod.env.js
+++ build/prod.env.js
@@ -1,9 +1,11 @@
 'use strict'
 const { execSync } = require('child_process');
+const path = require('path'); 
+const fs = require('fs');
 const md5 = require('md5');
-const stat = fs.statSync(path.join(__dirname, '.git'));
-const staticVersion = stat ? (execSync('git symbolic-ref --short -q HEAD', {})).toString().replace('\n', '') : 'test';
+const isExist = fs.existsSync(path.join( __dirname, '../.git'))
+const staticVersion = isExist ? (execSync('git symbolic-ref --short -q HEAD', {})).toString().replace('\n', '') : 'test';
 module.exports = {
-    PROJECT_NAME: "vue-h5-demo",
+    PROJECT_NAME: "vue-pc-demo",
     OUT_PATH: md5(staticVersion)
 }
